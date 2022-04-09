6583ad7d2d4311e752916d286fa270b86f80ffc9
Amy <redcardigan@live.com>
2022-04-09 20:28:23 +0100

    Implement clear button

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
-const square = []
+
 const container = document.querySelector('#container')
-const size = 16
-//const squares = document.querySelectorAll('.square')
+const btn = document.querySelector('#clear')
 
-/*Create grid divs*/
-for (let i = 0; i < size*size; i++) {
-    square[i] = document.createElement('div')
-    
-    container.appendChild(square[i])
-}
-console.log(square)
 
-square.forEach( (sqr) => {
-    console.log('hey')
-    sqr.addEventListener('mouseenter', () => {
-        console.log('mouse over')
-        sqr.classList.add('square')
-    })
+createGrid(16)
+
+
+/*Event listener for button*/
+btn.addEventListener('click', () => {
+    clearGrid()
+    createGrid(getSize())
 })
+
+
+function createGrid(size) {
+
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+    for (let i = 0; i < size * size; i++) {      
+        container.appendChild(document.createElement('div'))      
+    }
+
+    const squares = document.querySelectorAll('#container div')
+
+    /*Event listener for grid
+    Needs to be here because grid divs are created anew each time*/
+    squares.forEach( (sqr) => {
+        sqr.addEventListener('mouseenter', () => {
+        sqr.classList.add('square')
+        })
+    })
+}
+
+
+function getSize() {
+    console.log('Get size')
+    return window.prompt('Enter grid size (number of squares per side) - max 100:')
+    
+}
+
+
+function clearGrid() {
+    container.innerHTML = ''
+    console.log('Clear grid')
+}
+
+
+
+
