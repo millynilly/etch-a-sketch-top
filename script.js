604e6b307d70e90c508e783e36a8187e864ff746
Amy <redcardigan@live.com>
2022-04-11 15:50:30 +0100

    Improve shading effect implementation and refactor

--- script.js
+++ script.js
@@ -11,22 +11,16 @@ btnNew.addEventListener('click', () => {
     let size = getSize()
 
     if (size !== null) {
-        clearGrid()
+        clearSquares()
         createGrid(size) }
 })
 
-btnClear.addEventListener('click', () => {
-    let divs = document.querySelectorAll('#container div')
-
-    divs.forEach( (div) => {
-        div.style.backgroundColor = 'white'
-        div.setAttribute('data-colour', 'rgb(100%, 100%, 100%)')
-    })
-})
+btnClear.addEventListener('click', clearSquares)
 
 
 function createGrid(size) {
     
+    container.innerHTML = ''
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
     container.style.gridTemplateRows = `repeat(${size}, 1fr)`
 
@@ -34,11 +28,10 @@ function createGrid(size) {
         container.appendChild(document.createElement('div'))     
     }
     
-    /*Create data attribute for use with shading effect. Needs to be
-    in rgb and % format so can't use backgroundColor property.*/
-    let divs = document.querySelectorAll('#container div')
-    divs.forEach( (div) => div.setAttribute('data-colour', 'rgb(100%, 100%, 100%)'))
-    
+    /*Explicitly set background-color of divs to white.
+    For use with shading effect*/
+    clearSquares()
+
     /*Event listener for grid
     Needs to be here because grid divs are created anew each time*/
     const squares = document.querySelectorAll('#container div')
@@ -49,7 +42,7 @@ function createGrid(size) {
 function changeColour() {
 
     const effect = document.querySelector('input[name="effect"]:checked').value
-    const current = this.getAttribute('data-colour')
+    const current = this.style.backgroundColor
 
     switch (effect) {
         case 'grey':
@@ -60,7 +53,6 @@ function changeColour() {
             break
         case 'grey-scale':
             colour = getGreyScale(current)
-            this.setAttribute('data-colour', colour)
             break
     }
     this.style.backgroundColor = colour
@@ -78,8 +70,14 @@ function getSize() {
 }
 
 
-function clearGrid() {
-    container.innerHTML = ''
+function clearSquares() {
+    /*Sets background-color of all divs to white*/
+    
+    let divs = document.querySelectorAll('#container div')
+
+    divs.forEach( (div) => {
+        div.style.backgroundColor = 'rgb(255, 255, 255'
+    })
 }
 
 
@@ -97,12 +95,23 @@ function getRandomColour() {
 
 function getGreyScale(shade) {
     
-    /*Get rgb % & decreasee by 10*/
-    let n = shade.slice(4, 7)
-    n = n == 100 ? 90 : n.slice(0, 2) - 10
+    let greys = [255, 225, 200, 175, 150, 125, 100, 75, 50, 25, 0]
+
+
+    /*Get first rgb value. It's greyscale so r = g = b*/
+    let rgb = shade.slice(4, 7)
+
+    /*If black, return black.*/
+    if(rgb[0] == 0) { return 'rgb(0, 0, 0)' }
     
-    /*Put new % back into rgb string*/
-    return `rgb(${n}%, ${n}%, ${n}%)`
+    /*If under 100 (2 digits) remove comma.*/
+    rgb = rgb[rgb.length - 1] === ',' ? +rgb.slice(0, 2) : +rgb
+    
+    /*Get next value in greys array. ie. increase the shade.*/
+    rgb = greys[greys.indexOf(rgb) + 1]
+    
+    /*Put back into rgb string*/
+    return `rgb(${rgb}, ${rgb}, ${rgb})`
 }
 
 
